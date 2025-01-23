11704316a01f4fa5ad7d4828de452c5307dc981a
Nilesh Pratap Singh <ns458458@gmail.com>
2025-01-23 19:04:38 +0530

    modified new.js

--- new.js
+++ new.js
@@ -1,3 +1,27 @@
-let a = "3";
+let a = "nilesh";
 let b = 10;
-console.log(a+b);
+console.log(a + b);
+
+console.log(typeof a + typeof b);
+
+console.log(typeof (a + b));
+
+const c = {
+  name: "nilesh",
+  rollno: 5952,
+  section: true,
+};
+console.log(c);
+
+c["status"] = "passed";
+c["name"] = "rohan";
+console.log(c);
+
+const dict = {
+  word1: "defination",
+  word2: "defination",
+  word3: "defination",
+  word4: "defination",
+  word5: "defination",
+};
+console.log(dict);

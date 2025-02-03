feac3fb0bd815476eb53129984a14628b29e788c
Nilesh Pratap Singh <ns458458@gmail.com>
2025-02-04 01:51:55 +0530

    modified

--- for loop.js
+++ for loop.js
@@ -14,14 +14,14 @@ const prompt = require("prompt-sync")();
 // let d = factorial(num) / (factorial(r) * factorial(num - r));
 // console.log(`The ${num}C${r} is:`,d);
 
-let a = 20;
+/*let a = 20;
 for (let i = 0; i < a; i++) {
   c = i * a;
   console.log(c);
 }
 console.log("c" + c);
 d = a++;
-console.log(d);
+console.log(d+a);
 
 console.log(a);
 
@@ -35,3 +35,15 @@ const obj = {
 console.log(obj["name"]);
 console.log((obj["name"] = "Nilesh"));
 console.log(obj["name"]);
+
+for (let e in obj){
+  console.log(obj[e])
+}
+//console.log(e)*/
+
+let i = prompt("enter the value");
+while (i < 11) {
+   a = i + 11;
+  ++i;
+  console.log(a);
+}

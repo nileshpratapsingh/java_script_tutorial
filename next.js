b05b83de391682f82eb72593ad0c9fa1c49cfa31
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-04-10 01:05:32 +0530

    learning java script

---
+++ next.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')(); // Importing prompt-sync
+
+let a = prompt("enter value:")
+
+console.log(typeof a)
+console.log(a)
---
+++ tutorial.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")(); // Importing prompt-sync
+
+let friend = prompt("Enter your friend's name: "); // Prompting user to enter their friend's name
+let friend2 = "rajesh"
+
+console.log(`${friend} is the friend of ${friend2}`); // Printing a greeting message with the user's friend's 

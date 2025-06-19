37e14766ae0373efa0f8028a10efeea1bed2983a
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2025-06-19 16:18:48 +0530

    prototype.js

--- prototype.js
+++ prototype.js
@@ -16,3 +16,34 @@ child.__proto__ = parent;
 // Now child can access both methods
 child.sayHi();  // Output: "Hi from child!"
 child.greet();  // Output: "Hello from the prototype!"
+
+
+// new method
+
+const a = {
+    any() {
+        console.log("yo wass up");
+    }
+};
+
+const p = {
+    any2() {
+        console.log("Prototype of a");
+    }
+};
+
+// Preferred way:
+Object.setPrototypeOf(a, p);
+
+a.any();   // "yo wass up"
+a.any2();  // "Prototype of a"
+
+
+// creating the object prototype
+
+
+const proto = { greet() { console.log("Hello!"); } };
+
+const obj = Object.create(proto); // `proto` is the prototype of `obj`
+
+obj.greet(); // Output: Hello!
